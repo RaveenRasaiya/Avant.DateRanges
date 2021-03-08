6e020672758731818bf580e6dc001a7e7a024c5c
Raveen <vrraveen@gmail.com>
2021-03-08 23:14:05 +1100

    -add date validation

--- Avant.Application/DateRangeValidationService.cs
+++ Avant.Application/DateRangeValidationService.cs
@@ -7,13 +7,21 @@ namespace Avant.Application
     {
         public void Validate(DateTime startDate, DateTime endDate)
         {
+            if (startDate == DateTime.MinValue)
+            {
+                throw new ArgumentException($"Invalid {nameof(startDate)}");
+            }
+            if (endDate == DateTime.MinValue)
+            {
+                throw new ArgumentException($"Invalid {nameof(endDate)}");
+            }
             if (endDate.Date < startDate.Date)
             {
                 throw new ArgumentException($"{nameof(endDate)} should be greater than {nameof(startDate)}");
             }
             if (endDate.Date == startDate.Date)
             {
-                throw new ArgumentException($"{nameof(startDate)} can't be  {nameof(endDate)}");
+                throw new ArgumentException($"{nameof(endDate)} can't be same as {nameof(startDate)}");
             }
         }
     }
---
+++ Avant.Test/DateRangeValidationServiceTest.cs
@@ -0,0 +1,68 @@
+﻿using Avant.Application;
+using Avant.Application.Interfaces;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Avant.Test
+{
+    public class DateRangeValidationServiceTest
+    {
+      
+        private readonly IDateRangeValidationService _dateRangeValidationService;
+        public DateRangeValidationServiceTest()
+        {
+            _dateRangeValidationService = new DateRangeValidationService();          
+        }
+
+        [Fact]
+        public void DateRangeValidationService_StartDate_MinValue()
+        {
+            // arrange
+            Action act = () => _dateRangeValidationService.Validate(DateTime.MinValue, DateTime.Now.AddDays(-1));
+            //asert
+            act.Should().Throw<ArgumentException>().And.Message.Should().Be("Invalid startDate");
+        }
+
+
+        [Fact]
+        public void DateRangeValidationService_EndDate_MinValue()
+        {
+            // arrange
+            Action act = () => _dateRangeValidationService.Validate(DateTime.Now, DateTime.MinValue);
+            //asert
+            act.Should().Throw<ArgumentException>().And.Message.Should().Be("Invalid endDate");
+        }
+
+        [Fact]
+        public void DateRangeValidationService_InvalidDateRange()
+        {
+            // arrange
+            Action act = () => _dateRangeValidationService.Validate(DateTime.Now, DateTime.Now.AddDays(-1));
+            //asert
+            act.Should().Throw<ArgumentException>().And.Message.Should().Be("endDate should be greater than startDate");
+        }
+
+
+        [Fact]
+        public void DateRangeValidationService_SameDate()
+        {
+            // arrange
+            Action act = () => _dateRangeValidationService.Validate(DateTime.Now, DateTime.Now);
+            act.Should().Throw<ArgumentException>().And.Message.Should().Be("endDate can't be same as startDate");
+        }
+
+        [Fact]
+        public void DateRangeValidationService_Valid_DateRange()
+        {
+            // arrange
+            Action act = () => _dateRangeValidationService.Validate(DateTime.Now, DateTime.Now.AddDays(5));
+            act.Should().NotThrow<ArgumentException>();
+        }
+    }
+}
